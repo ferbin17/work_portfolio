015c0ce800985e7dfca82e766844bcccb96325a3
ferbin <ferbin17@gmail.com>
2020-11-04 05:16:46 +0530

    feat: folder creator script correction

--- folder_creator.sh
+++ folder_creator.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 # create sub directory for main app if needed
-directory=~/data/my_website/log/main_app
+directory=~/mf17/my_website/log/main_app
 if [ ! -d $directory ]; then
   mkdir $directory
 fi
 
 # create log file for cron if needed 
-CRON_FILE=~/data/my_website/log/main_app/main_app_cron.log
+CRON_FILE=~/mf17/my_website/log/main_app/main_app_cron.log
 if test -f "$CRON_FILE"; then
    :
 else
@@ -15,7 +15,7 @@ else
 fi
 
 # create log file for script if needed
-FILE=~/data/my_website/log/main_app/main_app.log
+FILE=~/mf17/my_website/log/main_app/main_app.log
 if test -f "$FILE"; then
    :
 else
@@ -23,7 +23,7 @@ else
 fi
 
 # create sub directory for todoapp if needed
-directory=~/data/my_website/vendor/engines/todoapp
+directory=~/mf17/my_website/vendor/engines/todoapp
 if [ -d $directory ]; then
   subdirectory=~/mf17/my_website/log/todoapp
   if [ ! -d $subdirectory ]; then
@@ -32,21 +32,21 @@ if [ -d $directory ]; then
 fi
 
 # create sub directory and log files for share_files_app if needed
-directory=~/data/my_website/vendor/engines/share_files_app
+directory=~/mf17/my_website/vendor/engines/share_files_app
 if [ -d $directory ]; then
   subdirectory=~/mf17/my_website/log/share_file_app
   if [ ! -d $subdirectory ]; then
     mkdir $subdirectory
   fi
   
-  CRON_FILE=~/data/my_website/log/share_file_app/rake_runner_cron.log
+  CRON_FILE=~/mf17/my_website/log/share_file_app/rake_runner_cron.log
   if test -f "$CRON_FILE"; then
      :
   else
      touch $CRON_FILE
   fi
   
-  FILE=~/data/my_website/log/share_file_app/rake_runner.log
+  FILE=~/mf17/my_website/log/share_file_app/rake_runner.log
   if test -f "$FILE"; then
     :
   else
